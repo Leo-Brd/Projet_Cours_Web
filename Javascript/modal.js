ad7ff20f254fee6f67a8c92de97ae03d9b59895c
Leo-Brd <leobordet.pro@gmail.com>
2024-02-09 14:16:24 +0100

    maj javascript

---
+++ Javascript/modal.js
@@ -0,0 +1,37 @@
+
+// Fonction pour afficher le modal
+function afficherModal_1() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("img01");
+    img.src = this.src; 
+    modal.style.display = "block"; 
+  }
+  
+    // Fermer le modal lorsqu'on clique sur le bouton de fermeture
+    document.querySelector(".close").addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+function afficherModal_2() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("img02");
+    img.src = this.src; 
+    modal.style.display = "block"; 
+  }
+  
+    // Fermer le modal lorsqu'on clique sur le bouton de fermeture
+    document.querySelector(".close").addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+function afficherModal_3() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("img03");
+    img.src = this.src; 
+    modal.style.display = "block"; 
+  }
+  
+    // Fermer le modal lorsqu'on clique sur le bouton de fermeture
+    document.querySelector(".close").addEventListener("click", function() {
+    modal.style.display = "none";
+});
